88019d1fc6aa8138aee5547af19dc7cafc02bb02
Hamad Raza <hamadmughal512@gmail.com>
2026-01-27 15:58:40 +0500

    cursor change

--- App.tsx
+++ App.tsx
@@ -23,13 +23,13 @@ const App: React.FC = () => {
     const cursorDot = cursorDotRef.current;
     
     const moveCursor = (e: MouseEvent) => {
-      gsap.to(cursor, {
+      gsap.set(cursor, {
         x: e.clientX,
         y: e.clientY,
         duration: 0.5,
         ease: 'power3.out'
       });
-      gsap.to(cursorDot, {
+      gsap.set(cursorDot, {
         x: e.clientX,
         y: e.clientY,
         duration: 0.1
@@ -38,10 +38,10 @@ const App: React.FC = () => {
 
     // Cursor hover effects
     const handleMouseEnter = () => {
-      gsap.to(cursor, { scale: 2.5, opacity: 0.5, duration: 0.3 });
+      gsap.set(cursor, { scale: 2.5, opacity: 0.5, duration: 0.3 });
     };
     const handleMouseLeave = () => {
-      gsap.to(cursor, { scale: 1, opacity: 1, duration: 0.3 });
+      gsap.set(cursor, { scale: 1, opacity: 1, duration: 0.3 });
     };
 
     window.addEventListener('mousemove', moveCursor);
